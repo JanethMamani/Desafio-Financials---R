bb30d696f784f33053e58dbff44127fcf46fedbb
Janeth Mamani Sacari <janeth@ibm.com>
2021-08-16 10:37:14 -0300

    Projeto de solução alternativa do Desafio Financials criado

---
+++ Expectativa-Mercado/src/main/java/com/fanfiction/expectativamercado/ExpectativaMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.fanfiction.expectativamercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpectativaMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpectativaMercadoApplication.class, args);
+	}
+
+}
---
+++ Expectativa-Mercado/src/test/java/com/fanfiction/expectativamercado/ExpectativaMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanfiction.expectativamercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpectativaMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
